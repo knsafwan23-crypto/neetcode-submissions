3ae9495ba6ab3c0f2d1c9f74f3482882591b685b
knsafwan23-crypto <knsafwan23@gmail.com>
2026-06-09 16:27:43 +0600

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static boolean hasDuplicate(int[] nums) {
+        for (int i=0 ;i<nums.length ;i ++) {
+            if(i<nums.length-1) {
+            for (int j=i+1; j<nums.length ; j++) {
+                if(nums[i]==nums[j]) {
+                    return true;
+                }
+            }
+            }
+            if (i>0) {
+            for (int j=i-1; j>0 ; j--) {
+                if(nums[i]==nums[j]) {
+                    return true;
+                }
+            }
+        }
+        }
+        return false ;
+    }
+    public static void main (String [] args) {
+System.out.print(hasDuplicate(new int [] {1, 2, 3, 3}));
+    }
+}
